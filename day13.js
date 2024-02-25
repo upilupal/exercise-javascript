44c09d64d110625b2f6fbeaf17ca7ddd3db8ba84
upilupal <naufalhanif891@gmail.com>
2024-02-25 13:49:03 +0700

    Exercise day13

--- day13.js
+++ day13.js
@@ -12,19 +12,54 @@
 // ● [Log] Queue 3 Done in XXX minutes
 // ● [Log] Queue 4 Done in XXX minutes
 
-class QueueHandler {
-    constructor() {
-      this.queue = [];
+// Food Ordering Queue
+
+class FoodOrderQueue {
+  constructor() {
+    this.orders = [];
+  }
+
+  showOrders() {
+    console.log("queue ", this.orders);
+  }
+
+  randomSec() {
+    return Math.floor(Math.random() * 4) + 1;
+  }
+
+  addOrder(order) {
+    this.orders.push(order);
+    console.log(`${order} added to queue`);
+  }
+
+  promise(data, sec) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`${data} Done in ${sec} seconds`);
+      }, sec * 1000);
+    });
+  }
+
+  async processOrders() {
+    let i = 0;
+    while (this.orders.length > 0) {
+      const result = await this.promise(this.orders[i], this.randomSec());
+      this.orders.shift();
+      console.log(result);
     }
 
-    enqueue(item) {
-        this.queue.push(item);
-        return (item) + ' telah dimasukkan'
-    }
-  
-    
+    this.showOrders();
   }
-  
-  const order = new QueueHandler();
-  console.log(order.enqueue("bakso"));
-  
+}
+
+const queue = new FoodOrderQueue();
+
+queue.addOrder("makanan1");
+queue.addOrder("makanan2");
+queue.addOrder("makanan3");
+queue.addOrder("makanan4");
+queue.addOrder("makanan5");
+
+queue.showOrders();
+
+queue.processOrders();
